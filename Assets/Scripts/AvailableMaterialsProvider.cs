dcb60ad2c21b4ac4d5c34edac8807f9db7a61b4c
Masha <107428174+Masha2022@users.noreply.github.com>
2023-01-11 17:53:23 +0300

    4

---
+++ Assets/Scripts/AvailableMaterialsProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AvailableMaterialsProvider : MonoBehaviour
+{
+    [SerializeField] private List<Material> _materials = new List<Material>();
+
+    public Material GetRandomMaterials()
+    {
+        return _materials[Random.Range(0, _materials.Count)];
+    }
+}
---
+++ Assets/Scripts/PaintController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PaintController : MonoBehaviour
+{
+    public void Colorize(PaintableObject target, Material material)
+    {
+        target.material = material;
+    }
+}
